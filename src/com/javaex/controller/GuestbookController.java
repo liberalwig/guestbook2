c1512dd5ac8221ec153cff51e9fcf11efec4608d
최승은 <liberalfilm@gmail.com>
2022-01-11 03:11:41 +0900

    2022.01.10(월)17:42 주석 달며 Model2 흐름·실행문 파악

--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -1,5 +1,5 @@
-//2022.01.10(월)17:42 상선 감독 하에 주석 달아 파악하기
-package com.javaex.servlet;
+//2022.01.10(월)17:42 상선 감독 하에 주석 달아 파악하기//2022.01.10(월)23:30_skimming
+package com.javaex.controller;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,7 +27,10 @@ public class GuestbookController extends HttpServlet {
 		String action = request.getParameter("action"); //GuestbookVo의 파라미터를 뽑아 쓴다 -- 주소의 action 파라미터 값을 문자열로 가져온다.
 		//ex /gbc?action=addlist&name=유재석
 		
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////		
+		
+		
+		
+		
 
 		if ("addlist".equals(action)) { //유저가 addList페이지를 원했다면(= value 입력할 페이지를 보여 주려면)
 			System.out.println("action=addlist");
@@ -38,8 +41,11 @@ public class GuestbookController extends HttpServlet {
 			//forward 할때, rd에서 지정한 위치로 일을 넘긴다.
 			//포워드 할때 특정한 값을 같이 넘겨주고 싶을때 어트리뷰트에 값을 저장해서 보내 줄 수 있다 (자료형은 상관 없이 다 가능)
 			//이 값을 뽑아낼때, 형변환을 해야한다.
+
 			
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			
+			
+			
 
 		} else if ("add".equals(action)) { //유저가 add페이지 원했다면(= 유저가 value를 입력해서 다음으로 넘겨 줘야 한다면)
 			System.out.println("action=add");
@@ -53,7 +59,10 @@ public class GuestbookController extends HttpServlet {
 			System.out.println(count + "건 등록되었습니다"); //위의 건수를 출력해준다. '음,유저 둘이 등록한 대로 두 건이 잘 올라갔군'
 			response.sendRedirect("/guestbook2/gbc?action=addlist");
 
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+			
+			
+			
 			
 		} else if ("deleteform".equals(action)) { //유저가 deleteform페이지 원했다면
 			System.out.println("action=deleteform");
@@ -64,7 +73,10 @@ public class GuestbookController extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/deleteForm.jsp"); //그 일을 할 jsp파일의 위치를 찍어준다
 			rd.forward(request, response); //forward방식 거쳐서 request에 대한 request를 유저에게 내놓는다
 
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+			
+			
+			
 			
 		} else if ("delete".equals(action)) {
 			System.out.println("action=delete");
@@ -82,7 +94,9 @@ public class GuestbookController extends HttpServlet {
 			//리다이렉트
 			response.sendRedirect("/guestbook2/gbc?action=addlist");
 
-		//////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+			
+			
+			
 			
 		} else {
 			System.out.println("파라미터 없음");
